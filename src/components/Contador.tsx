feaf2667acf07f057dadc4cb032b753b4d60cbec
Luis Colorado <luis.colorado@laika.com.co>
2022-01-05 16:26:39 -0500

    click Counter added on Contador component

--- src/components/Contador.tsx
+++ src/components/Contador.tsx
@@ -1,8 +1,25 @@
+import { useState } from "react"
 
 export const Contador = () => {
+const [valor, setValor] = useState(10)
+    const contar = (numero:number)=>{
+        setValor(valor+numero)
+
+    }
     return (
         <>
-           <h3>Contador</h3> 
+           <h3>Contador: <small>{valor}</small></h3> 
+
+           <button 
+           className="btn btn-primary"
+           onClick={()=>contar(-1)}
+           >-1</button>
+           &nbsp;
+           <button 
+           className="btn btn-primary"
+           onClick={()=>contar(+1)}
+           >
+               +1</button>
         </>
     )
 }
